635a84794137e903a1562e80b940584ef807f199
layanalshammari4j <hadise.layan@gmail.com>
2026-08-16 21:53:15 +0300

    remove sql server migrations

--- MyCollegeEvents/obj/Debug/net8.0/MyCollegeEvents.AssemblyInfo.cs
+++ MyCollegeEvents/obj/Debug/net8.0/MyCollegeEvents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c72ded8ba5790b6a031dd148081c5ffcbdc74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9bd60c51fa77f3fcc884a853a24825c428b14de")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
