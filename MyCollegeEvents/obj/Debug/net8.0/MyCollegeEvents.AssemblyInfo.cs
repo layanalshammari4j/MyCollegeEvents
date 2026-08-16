ec36bbdd68b2422f40ee83fb42ca376ab3b11ac0
layanalshammari4j <hadise.layan@gmail.com>
2026-08-16 22:05:55 +0300

    remove automatic table creation to fix startup crash

--- MyCollegeEvents/obj/Debug/net8.0/MyCollegeEvents.AssemblyInfo.cs
+++ MyCollegeEvents/obj/Debug/net8.0/MyCollegeEvents.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635a84794137e903a1562e80b940584ef807f199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39fa7e6bc6bc104fc2a77b1a2cb9dcf8ab77da6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCollegeEvents")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
