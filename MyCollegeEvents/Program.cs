478111126e9da6075e9654af4e94cc775065bf07
Layan4w <hadise.layan@gmail.com>
2026-08-18 20:51:35 +0300

    Update Program.cs

--- MyCollegeEvents/Program.cs
+++ MyCollegeEvents/Program.cs
@@ -35,7 +35,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseHsts();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
